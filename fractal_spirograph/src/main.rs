889b18ef7b0a85095e533971f1f8fef0d385a023
Kasun Liyanage <kasun14111indika@gmail.com>
2022-09-19 08:42:14 +0530

    Configured based drawing

--- fractal_spirograph/src/main.rs
+++ fractal_spirograph/src/main.rs
@@ -1,153 +1,443 @@
-use bevy::{prelude::*, render::camera::RenderTarget};
-use bevy_prototype_lyon::prelude::*;
+use wgpu::util::DeviceExt;
+use winit::{
+    dpi::{LogicalSize, PhysicalSize, Size},
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window::Window,
+    window::WindowBuilder,
+};
 
-const WINDOW_HEIGHT: f32 = 1000.;
-const WINDOW_WIDTH: f32 = 1000.;
+use rand::Rng;
 
-#[derive(Component)]
-pub struct MainCamera;
-
-pub struct WinSize {
-    pub h: f32,
-    pub w: f32,
+// We need this for Rust to store our data correctly for the shaders
+#[repr(C)]
+// This is so we can store this in a buffer
+#[derive(Debug, Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
+struct TransUniform {
+    translation: [f32; 4],
 }
 
-pub struct CursorPosition {
-    pub x: f32,
-    pub y: f32,
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+struct Vertex {
+    position: [f32; 3],
+    color: [f32; 3],
 }
 
-#[derive(Component)]
-pub struct AreaBoundary_1;
+impl Vertex {
+    pub fn new() -> Self {
+        Vertex {
+            position: [0., 0., 0.],
+            color: [1., 0., 0.],
+        }
+    }
 
-#[derive(Component)]
-pub struct AngleBase_1(f64);
+    fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {
+        wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &[
+                wgpu::VertexAttribute {
+                    offset: 0,
+                    shader_location: 0,
+                    format: wgpu::VertexFormat::Float32x3,
+                },
+                wgpu::VertexAttribute {
+                    offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
+                    shader_location: 1,
+                    format: wgpu::VertexFormat::Float32x3,
+                },
+            ],
+        }
+    }
+}
 
-#[derive(Component)]
-pub struct AreaBoundary_2;
+pub struct DrawVec(Vec<Vertex>);
 
-#[derive(Component)]
-pub struct AngleBase_2(f64);
+pub fn run() {
+    env_logger::init();
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_title("Hello Wgpu")
+        .build(&event_loop)
+        .unwrap();
 
-#[derive(Component)]
-pub struct AreaBoundary_3;
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent {
+            ref event,
+            window_id,
+        } if window_id == window.id() => match event {
+            WindowEvent::CloseRequested
+            | WindowEvent::KeyboardInput {
+                input:
+                    KeyboardInput {
+                        state: ElementState::Pressed,
+                        virtual_keycode: Some(VirtualKeyCode::Escape),
+                        ..
+                    },
+                ..
+            } => *control_flow = ControlFlow::Exit,
+            _ => {}
+        },
+        _ => {}
+    });
+}
 
-#[derive(Component)]
-pub struct AngleBase_3(f64);
+pub async fn run_again() {
+    // Window setup...
+    env_logger::init();
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_title("Hello Wgpu")
+        .build(&event_loop)
+        .unwrap();
+
+    let mut state = State::new(&window).await;
+
+    event_loop.run(move |event, _, control_flow| {
+        match event {
+            Event::WindowEvent {
+                ref event,
+                window_id,
+            } if window_id == window.id() => {
+                if !state.input(event) {
+                    match event {
+                        WindowEvent::CloseRequested
+                        | WindowEvent::KeyboardInput {
+                            input:
+                                KeyboardInput {
+                                    state: ElementState::Pressed,
+                                    virtual_keycode: Some(VirtualKeyCode::Escape),
+                                    ..
+                                },
+                            ..
+                        } => *control_flow = ControlFlow::Exit,
+                        WindowEvent::Resized(physical_size) => {
+                            state.resize(*physical_size);
+                        }
+                        WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                            // new_inner_size is &&mut so w have to dereference it twice
+                            state.resize(**new_inner_size);
+                        }
+                        _ => {}
+                    }
+                }
+            }
+            Event::RedrawRequested(window_id) if window_id == window.id() => {
+                state.update();
+                match state.render() {
+                    Ok(_) => {}
+                    // Reconfigure the surface if it's lost or outdated
+                    Err(wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated) => {
+                        state.resize(state.size)
+                    }
+                    // The system is out of memory, we should probably quit
+                    Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+
+                    Err(wgpu::SurfaceError::Timeout) => log::warn!("Surface timeout"),
+                }
+            }
+            Event::RedrawEventsCleared => {
+                // RedrawRequested will only trigger once, unless we manually
+                // request it.
+                window.request_redraw();
+            }
+            _ => {}
+        }
+    });
+
+    // Event loop...
+}
+
+struct State {
+    surface: wgpu::Surface,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    size: winit::dpi::PhysicalSize<u32>,
+    srender_pipeline: wgpu::RenderPipeline,
+    svertex_buffer: wgpu::Buffer,
+    index: i32,
+    current_index: i32,
+    angle_1: f64,
+    angle_2: f64,
+    angle_3: f64,
+    angle_4: f64,
+    angle_5: f64,
+    trans_x: f32,
+    trans_y: f32,
+}
+
+impl State {
+    // Creating some of the wgpu types requires async code
+    async fn new(window: &Window) -> Self {
+        let size = window.inner_size();
+
+        // The instance is a handle to our GPU
+        // Backends::all => Vulkan + Metal + DX12 + Browser WebGPU
+        let instance = wgpu::Instance::new(wgpu::Backends::all());
+        let surface = unsafe { instance.create_surface(window) };
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: false,
+            })
+            .await
+            .unwrap();
+
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    features: wgpu::Features::empty(),
+                    // WebGL doesn't support all of wgpu's features, so if
+                    // we're building for the web we'll have to disable some.
+                    limits: if cfg!(target_arch = "wasm32") {
+                        wgpu::Limits::downlevel_webgl2_defaults()
+                    } else {
+                        wgpu::Limits::default()
+                    },
+                    label: None,
+                },
+                None, // Trace path
+            )
+            .await
+            .unwrap();
+
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface.get_supported_formats(&adapter)[0],
+            width: size.width,
+            height: size.height,
+            present_mode: wgpu::PresentMode::Fifo,
+        };
+        surface.configure(&device, &config);
+
+        let sshader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+            label: Some("Shader"),
+            source: wgpu::ShaderSource::Wgsl(include_str!("sbuffer_shader.wgsl").into()),
+        });
+
+        let render_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: Some("Render Pipeline Layout"),
+                bind_group_layouts: &[],
+                push_constant_ranges: &[],
+            });
+
+        let srender_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: Some("Render Pipeline Layout"),
+                bind_group_layouts: &[],
+                push_constant_ranges: &[],
+            });
+
+        let srender_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Render Pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &sshader,
+                entry_point: "vs_main",     // 1.
+                buffers: &[Vertex::desc()], // 2.
+            },
+            fragment: Some(wgpu::FragmentState {
+                // 3.
+                module: &sshader,
+                entry_point: "fs_main",
+                targets: &[Some(wgpu::ColorTargetState {
+                    // 4.
+                    format: config.format,
+                    blend: Some(wgpu::BlendState::REPLACE),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::LineStrip, // 1.
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw, // 2.
+                cull_mode: Some(wgpu::Face::Back),
+                // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
+                polygon_mode: wgpu::PolygonMode::Fill,
+                // Requires Features::DEPTH_CLIP_CONTROL
+                unclipped_depth: false,
+                // Requires Features::CONSERVATIVE_RASTERIZATION
+                conservative: false,
+            }, // 5.
+            depth_stencil: None, // 1.
+            multisample: wgpu::MultisampleState {
+                count: 1,                         // 2.
+                mask: !0,                         // 3.
+                alpha_to_coverage_enabled: false, // 4.
+            },
+            multiview: None,
+        });
+
+        let mut inner_vec = Vec::new();
+
+        for i in 0..10000 {
+            inner_vec.push(Vertex::new())
+        }
+
+        let num_vertices = inner_vec.len() as u32;
+        let current_xy = [0., 0.];
+
+        let draw_vec = DrawVec(inner_vec);
+        let index = 0;
+
+        let svertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Vertex Buffer Draw"),
+            contents: bytemuck::cast_slice(draw_vec.0.as_slice()),
+            usage: wgpu::BufferUsages::VERTEX | wgpu::BufferUsages::COPY_DST,
+        });
+
+        let current_index = 0;
+
+        let angle_1 = 0.;
+        let angle_2 = 0.;
+        let angle_3 = 0.;
+        let angle_4 = 0.;
+        let angle_5 = 0.;
+
+        let trans_x = 0.;
+        let trans_y = 0.;
+
+        Self {
+            surface,
+            device,
+            queue,
+            config,
+            size,
+            srender_pipeline,
+            svertex_buffer,
+            index,
+            current_index,
+            angle_1,
+            angle_2,
+            angle_3,
+            angle_4,
+            angle_5,
+            trans_x,
+            trans_y,
+        }
+    }
+
+    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        if new_size.width > 0 && new_size.height > 0 {
+            self.size = new_size;
+            self.config.width = new_size.width;
+            self.config.height = new_size.height;
+            self.surface.configure(&self.device, &self.config);
+        }
+    }
+
+    fn input(&mut self, event: &WindowEvent) -> bool {
+        false
+    }
+
+    fn update(&mut self) {
+        let trfm_1_translation_x = (0. + self.angle_1.sin() * 0.5) as f32;
+        let trfm_1_translation_y = (0. + self.angle_1.cos() * 0.5) as f32;
+
+        let trfm_2_translation_x = (trfm_1_translation_x as f64 + self.angle_2.sin() * 0.25) as f32;
+        let trfm_2_translation_y = (trfm_1_translation_y as f64 + self.angle_2.cos() * 0.25) as f32;
+
+        let trfm_3_translation_x =
+            (trfm_1_translation_x as f64 + self.angle_3.sin() * 0.125) as f32;
+        let trfm_3_translation_y =
+            (trfm_1_translation_y as f64 + self.angle_3.cos() * 0.125) as f32;
+
+        let trfm_4_translation_x = (trfm_1_translation_x as f64 + self.angle_4.sin() * 0.25) as f32;
+        let trfm_4_translation_y = (trfm_1_translation_y as f64 + self.angle_4.cos() * 0.25) as f32;
+
+        self.trans_x = trfm_4_translation_x;
+        self.trans_y = trfm_4_translation_y;
+
+        self.angle_1 = self.angle_1 + 0.1;
+        self.angle_2 = self.angle_2 + 0.1;
+        self.angle_3 = self.angle_3 + 0.1;
+        self.angle_4 = self.angle_4 + 0.1;
+    }
+
+    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        let output = self.surface.get_current_texture()?;
+        let view = output
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
+            });
+
+        if self.index % 5 == 0 {
+            let mut inner_vec = vec![Vertex::new(); 10000];
+
+            let constant = self.index as f32 * 0.0001;
+
+            let mut rng = rand::thread_rng();
+
+            inner_vec[self.current_index as usize] = Vertex {
+                position: [self.trans_x, self.trans_y, 0.0],
+                color: [0.0, 0.0, 0.0],
+            };
+
+            let new_buffer = self
+                .device
+                .create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                    label: Some("Vertex Buffer Draw"),
+                    contents: bytemuck::cast_slice(inner_vec.as_slice()),
+                    usage: wgpu::BufferUsages::COPY_SRC,
+                });
+
+            encoder.copy_buffer_to_buffer(
+                &new_buffer,
+                (self.current_index * 24) as u64,
+                &self.svertex_buffer,
+                (self.current_index * 24) as u64,
+                (1 * 24) as u64,
+            );
+
+            self.current_index += 1;
+            println!("{}", self.current_index);
+        }
+        self.index += 1;
+
+        {
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[
+                    // This is what @location(0) in the fragment shader targets
+                    Some(wgpu::RenderPassColorAttachment {
+                        view: &view,
+                        resolve_target: None,
+                        ops: wgpu::Operations {
+                            load: wgpu::LoadOp::Clear(wgpu::Color {
+                                r: 0.98,
+                                g: 0.98,
+                                b: 0.98,
+                                a: 1.0,
+                            }),
+                            store: true,
+                        },
+                    }),
+                ],
+                depth_stencil_attachment: None,
+            });
+
+            render_pass.set_pipeline(&self.srender_pipeline);
+            render_pass.set_vertex_buffer(0, self.svertex_buffer.slice(..));
+            render_pass.draw(0..(self.current_index as u32), 0..1);
+        }
+
+        // submit will accept anything that implements IntoIter
+        self.queue.submit(std::iter::once(encoder.finish()));
+        output.present();
+
+        Ok(())
+    }
+}
 
 fn main() {
-    App::new()
-        .insert_resource(ClearColor(Color::rgb(0.9, 0.9, 0.9)))
-        .insert_resource(WindowDescriptor {
-            title: "Fractal Spirograph".to_string(),
-            width: WINDOW_WIDTH,
-            height: WINDOW_HEIGHT,
-            ..Default::default()
-        })
-        .add_plugins(DefaultPlugins)
-        .add_plugin(ShapePlugin)
-        .add_startup_system(setup_system)
-        .add_system(update_system)
-        .run();
-}
-
-fn setup_system(mut commands: Commands, mut windows: ResMut<Windows>) {
-    commands
-        .spawn()
-        .insert_bundle(Camera2dBundle::default())
-        .insert(MainCamera);
-
-    let window = windows.get_primary_mut().unwrap();
-    let (WinW, WinH) = (window.width(), window.height());
-
-    let win_size = WinSize { h: WinH, w: WinW };
-    commands.insert_resource(win_size);
-
-    let shape_1 = shapes::Circle {
-        radius: 125.,
-        center: Vec2::new(0., 0.),
-    };
-
-    commands.spawn_bundle(GeometryBuilder::build_as(
-        &shape_1,
-        DrawMode::Stroke(StrokeMode::new(Color::BLUE, 2.0)),
-        Transform::default(),
-    ));
-
-    let shape_2 = shapes::Circle {
-        radius: 75.,
-        center: Vec2::new(0., 0.),
-    };
-
-    commands
-        .spawn_bundle(GeometryBuilder::build_as(
-            &shape_2,
-            DrawMode::Stroke(StrokeMode::new(Color::RED, 2.0)),
-            Transform::default(),
-        ))
-        .insert(AreaBoundary_1)
-        .insert(AngleBase_1(0.));
-
-    let shape_3 = shapes::Circle {
-        radius: 35.,
-        center: Vec2::new(0., 0.),
-    };
-
-    commands
-        .spawn_bundle(GeometryBuilder::build_as(
-            &shape_3,
-            DrawMode::Stroke(StrokeMode::new(Color::RED, 2.0)),
-            Transform::default(),
-        ))
-        .insert(AreaBoundary_2)
-        .insert(AngleBase_2(0.));
-
-    let shape_4 = shapes::Circle {
-        radius: 15.,
-        center: Vec2::new(0., 0.),
-    };
-
-    commands
-        .spawn_bundle(GeometryBuilder::build_as(
-            &shape_4,
-            DrawMode::Stroke(StrokeMode::new(Color::RED, 2.0)),
-            Transform::default(),
-        ))
-        .insert(AreaBoundary_3)
-        .insert(AngleBase_3(0.));
-}
-
-fn update_system(
-    mut commands: Commands,
-    mut boundary_query_1: Query<
-        (&mut Transform, &mut AngleBase_1),
-        (
-            With<AreaBoundary_1>,
-            Without<AreaBoundary_2>,
-            Without<AreaBoundary_3>,
-        ),
-    >,
-    mut boundary_query_2: Query<
-        (&mut Transform, &mut AngleBase_2),
-        (With<AreaBoundary_2>, Without<AreaBoundary_3>),
-    >,
-    mut boundary_query_3: Query<(&mut Transform, &mut AngleBase_3), With<AreaBoundary_3>>,
-) {
-    let (mut trfm_1, mut angle_1) = boundary_query_1.get_single_mut().unwrap();
-    // trfm.translation.x = trfm.translation.x + 0.1;
-    // trfm.translation.y = trfm.translation.y + 0.1;
-    trfm_1.translation.x = (0. + angle_1.0.sin() * 200.) as f32;
-    trfm_1.translation.y = (0. + angle_1.0.cos() * 200.) as f32;
-
-    let (mut trfm_2, mut angle_2) = boundary_query_2.get_single_mut().unwrap();
-    trfm_2.translation.x = (trfm_1.translation.x as f64 + angle_2.0.sin() * 110.) as f32;
-    trfm_2.translation.y = (trfm_1.translation.y as f64 + angle_2.0.cos() * 110.) as f32;
-
-    let (mut trfm_3, mut angle_3) = boundary_query_3.get_single_mut().unwrap();
-    trfm_3.translation.x = (trfm_2.translation.x as f64 + angle_3.0.sin() * 50.) as f32;
-    trfm_3.translation.y = (trfm_2.translation.y as f64 + angle_3.0.cos() * 50.) as f32;
-
-    angle_1.0 = angle_1.0 + 0.05;
-    angle_2.0 = angle_2.0 + 0.1;
-    angle_3.0 = angle_3.0 + 0.6;
+    pollster::block_on(run_again());
 }
